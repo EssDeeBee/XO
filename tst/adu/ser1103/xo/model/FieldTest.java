ad5402306baad9973258c9e78b8b708ea40cb98d
ser1103 <dzeboevsb@gmail.com>
2017-08-20 10:33:15 +0300

    FieldTests checks for the exceptions have been added

--- tst/adu/ser1103/xo/model/FieldTest.java
+++ tst/adu/ser1103/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package adu.ser1103.xo.model;
 
+import adu.ser1103.xo.model.exceptions.AlreadyOccupiedException;
 import adu.ser1103.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -26,6 +27,21 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
+    }
+
+    @Test
+    public void testSetFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
+
     }
 
     @Test
